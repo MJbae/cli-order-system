cb71de1ffcc3cb98d11a6d1a364865596b3499f1
MJbae <bmj5208@gmail.com>
2022-11-16 22:59:49 +0900

    주문 명령에 대한 happy path 생성

--- src/main/java/kr/co/_29cm/homework/cli/Command.java
+++ src/main/java/kr/co/_29cm/homework/cli/Command.java
@@ -1,5 +1,7 @@
 package kr.co._29cm.homework.cli;
 
+import kr.co._29cm.homework.OrderService;
+import kr.co._29cm.homework.domain.Order;
 import org.jline.utils.InputStreamReader;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
@@ -12,17 +14,19 @@ import java.io.IOException;
 @ShellComponent
 public class Command implements Quit.Command {
     private final Printer printer;
+    private final OrderService service;
 
-    public Command(Printer printer) {
+    public Command(Printer printer, OrderService service) {
         this.printer = printer;
+        this.service = service;
     }
 
     @ShellMethod(key = {"order", "o"}, value = "order")
     public void order() {
         printer.printItemsAll();
 
-        String itemInput;
-        String countInput;
+        Long itemId = null;
+        Integer itemCount = 0;
 
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -30,15 +34,22 @@ public class Command implements Quit.Command {
 
             while (true) {
                 printer.itemPrompt();
-                itemInput = reader.readLine();
+                String itemInput = reader.readLine();
+                if (!itemInput.equals(" ")){
+                    itemId = Long.getLong(itemInput);
+                }
                 printer.printItemOrdered(itemInput);
 
                 printer.countPrompt();
-                countInput = reader.readLine();
+                String countInput = reader.readLine();
+                if (!countInput.equals(" ")){
+                    itemCount = Integer.getInteger(countInput);
+                }
                 printer.printCountOrdered(countInput);
 
                 if (itemInput.equals(" ") && countInput.equals(" ")) {
-                    printer.printItemsOrdered();
+                    Order order = service.order(itemId, itemCount);
+                    printer.printItemsOrdered(order);
                     break;
                 }
             }
