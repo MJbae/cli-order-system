d89e52ca070fc10990ffc9fa875db2ef38332df5
MJbae <bmj5208@gmail.com>
2022-11-17 08:33:32 +0900

    OrderDto 생성

---
+++ src/main/java/kr/co/_29cm/homework/OrderDto.java
@@ -0,0 +1,14 @@
+package kr.co._29cm.homework;
+
+import lombok.Getter;
+
+@Getter
+public class OrderDto {
+    private final Long itemId;
+    private final Integer itemCount;
+
+    public OrderDto(Long itemId, Integer itemCount){
+        this.itemId = itemId;
+        this.itemCount = itemCount;
+    }
+}
