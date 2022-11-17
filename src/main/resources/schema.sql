f875425cd239a76a1e205d612a59d94e28842e12
배만진 <111344711+mj-at-ogq@users.noreply.github.com>
2022-11-17 13:36:45 +0900

    order, order_item에 대한 DDL 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,21 @@ CREATE TABLE item
     stock_quantity INT    NOT NULL,
     CONSTRAINT pk_item PRIMARY KEY (item_id)
 );
+
+CREATE TABLE `order`
+(
+    order_id        BIGINT NOT NULL,
+    price           INT    NOT NULL,
+    CONSTRAINT pk_order PRIMARY KEY (order_id)
+);
+
+CREATE TABLE order_item
+(
+    order_item_id   BIGINT    NOT NULL,
+    order_id        BIGINT    NOT NULL,
+    item_id         BIGINT    NOT NULL,
+    count           INT       NOT NULL,
+    CONSTRAINT pk_order_item PRIMARY KEY (order_item_id),
+    foreign key (order_id) references `order`(order_id),
+    foreign key (item_id) references item(item_id)
+);
