a02e4f6c03a2887403cd69f5f218524f7774a62b
MJbae <bmj5208@gmail.com>
2022-11-18 06:55:52 +0900

    Prompt 인터페이스 정의

---
+++ src/main/java/kr/co/_29cm/homework/cli/interfaces/Prompt.java
@@ -0,0 +1,16 @@
+package kr.co._29cm.homework.cli.interfaces;
+
+import kr.co._29cm.homework.cli.ItemIdPrompt;
+import kr.co._29cm.homework.cli.OrderCountPrompt;
+
+/**
+ * 사용자 입력에 대한 대기 상태를 표시한다
+ * <p>
+ * All Known Implementing Classes:
+ * @see ItemIdPrompt
+ * @see OrderCountPrompt
+ * </p>
+ */
+public interface Prompt {
+    void display();
+}
