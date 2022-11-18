91a7a9ae553f602db5e0f2d6d27fa4cb414644b0
배만진 <111344711+mj-at-ogq@users.noreply.github.com>
2022-11-18 10:58:08 +0900

    주문 조건 OrderItem 엔티티 조회 메소드 추가

--- src/main/java/kr/co/_29cm/homework/OrderItemRepository.java
+++ src/main/java/kr/co/_29cm/homework/OrderItemRepository.java
@@ -1,7 +1,11 @@
 package kr.co._29cm.homework;
 
+import kr.co._29cm.homework.domain.Order;
 import kr.co._29cm.homework.domain.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findByOrder(Order order);
 }
