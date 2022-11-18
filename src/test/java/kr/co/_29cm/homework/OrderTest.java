5f6ed6073c2af351a740a5a41ea3b41221015aaa
MJbae <bmj5208@gmail.com>
2022-11-18 22:36:52 +0900

    refactor: 로직의 분기를 효과적으로 테스트하기 위해 중첩 구조로 변경

--- src/test/java/kr/co/_29cm/homework/OrderTest.java
+++ src/test/java/kr/co/_29cm/homework/OrderTest.java
@@ -9,6 +9,8 @@ import kr.co._29cm.homework.domain.OrderItem;
 import kr.co._29cm.homework.infra.OrderItemRepository;
 import kr.co._29cm.homework.infra.OrderRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +23,7 @@ import static org.mockito.Mockito.mock;
 import java.math.BigDecimal;
 import java.util.List;
 
+@DisplayName("OrderService")
 @Transactional
 class OrderTest {
     private OrderService orderService;
@@ -32,42 +35,75 @@ class OrderTest {
     private final Order orderMocking = mock(Order.class);
     private Item item;
     private OrderItem orderItem;
-    private final int orderCount = 1;
+    private int orderCount;
+    private BigDecimal itemPrice;
     private final Long itemId = 778422L;
     private final String itemName = "캠핑덕 우드롤테이블";
     private final int stockQuantity = 7;
-    private final BigDecimal itemPrice = BigDecimal.valueOf(45000);
     private final BigDecimal deliveryFee = BigDecimal.valueOf(2500);
 
     @BeforeEach
     void setUp() {
         orderService = new OrderService(orderRepository, orderItemRepository, itemService);
         orderItemService = new OrderItemService(orderItemRepository);
-        item = new Item(itemId, itemName, itemPrice, stockQuantity);
-        orderItem = new OrderItem(orderMocking, item, orderCount);
-
-        given(itemService.loadOne(any())).willReturn(item);
-        given(orderItemRepository.findByOrder(any())).willReturn(List.of());
-
     }
 
-    @Test
-    public void 주문금액이_5만원_이상이면_지불금액에_배송료가_붙지않는다() {
-        Order order = orderService.order(itemId, orderCount);
+    @Nested
+    @DisplayName("order 메소드는")
+    class Describe_order {
 
-        BigDecimal actualPrice = order.getPrice();
-        BigDecimal expectedPrice = itemPrice.multiply(BigDecimal.valueOf(orderCount));
+        @Nested
+        @DisplayName("만약 주문금액이_5만원_이상이면")
+        class Context_with_order_price_higher_than_or_equal_to_50000 {
+            @BeforeEach
+            void setUp() {
+                orderCount = 2;
+                itemPrice = BigDecimal.valueOf(45000);
 
-        assertThat(actualPrice).isEqualTo(expectedPrice);
-    }
+                item = new Item(itemId, itemName, itemPrice, stockQuantity);
+                orderItem = new OrderItem(orderMocking, item, orderCount);
 
-    @Test
-    public void 주문금액이_5만원_미만이면_지불금액에_배송료가_붙는다() {
-        Order order = orderService.order(itemId, orderCount);
+                given(itemService.loadOne(any())).willReturn(item);
+                given(orderItemRepository.findByOrder(any())).willReturn(List.of(orderItem));
+            }
 
-        BigDecimal actualPrice = order.getPrice();
-        BigDecimal expectedPrice = itemPrice.multiply(BigDecimal.valueOf(orderCount * 2)).add(deliveryFee);
+            @Test
+            @DisplayName("주문 엔티티의 price에 배송료를 포함하지 않고 반환한다")
+            void it_returns_price_not_including_delivery_fee() {
+                Order order = orderService.order(itemId, orderCount);
 
-        assertThat(actualPrice).isEqualTo(expectedPrice);
+                BigDecimal actualPrice = order.getPrice();
+                BigDecimal expectedPrice = itemPrice.multiply(BigDecimal.valueOf(orderCount));
+
+                assertThat(actualPrice).isEqualTo(expectedPrice);
+            }
+        }
+
+        @Nested
+        @DisplayName("만약 주문금액이_5만원_미만이면")
+        class Context_with_order_price_lower_than_50000 {
+            @BeforeEach
+            void setUp() {
+                orderCount = 1;
+                itemPrice = BigDecimal.valueOf(45000);
+
+                item = new Item(itemId, itemName, itemPrice, stockQuantity);
+                orderItem = new OrderItem(orderMocking, item, orderCount);
+
+                given(itemService.loadOne(any())).willReturn(item);
+                given(orderItemRepository.findByOrder(any())).willReturn(List.of(orderItem));
+            }
+
+            @Test
+            @DisplayName("주문 엔티티의 price에 배송료를 포함하여 반환한다")
+            void it_returns_price_including_delivery_fee() {
+                Order order = orderService.order(itemId, orderCount);
+
+                BigDecimal actualPrice = order.getPrice();
+                BigDecimal expectedPrice = itemPrice.multiply(BigDecimal.valueOf(orderCount)).add(deliveryFee);
+
+                assertThat(actualPrice).isEqualTo(expectedPrice);
+            }
+        }
     }
 }
