75cb1fa576a87ce64894c2db3742a5f951684d3b
배만진 <111344711+mj-at-ogq@users.noreply.github.com>
2022-11-18 15:56:03 +0900

    fix: 싱글톤 활용 객체에 대해 Component 어노테이션 추가

--- src/main/java/kr/co/_29cm/homework/cli/ItemIdPrompt.java
+++ src/main/java/kr/co/_29cm/homework/cli/ItemIdPrompt.java
@@ -1,7 +1,9 @@
 package kr.co._29cm.homework.cli;
 
 import kr.co._29cm.homework.cli.interfaces.Prompt;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ItemIdPrompt implements Prompt {
     public final String PRODUCT_PROMPT = "상품번호: ";
 
--- src/main/java/kr/co/_29cm/homework/cli/OrderCountPrompt.java
+++ src/main/java/kr/co/_29cm/homework/cli/OrderCountPrompt.java
@@ -1,9 +1,11 @@
 package kr.co._29cm.homework.cli;
 
 import kr.co._29cm.homework.cli.interfaces.Prompt;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderCountPrompt implements Prompt {
-    public static final String ORDER_COUNT_PROMPT = "주문수량: ";
+    public final String ORDER_COUNT_PROMPT = "주문수량: ";
 
     @Override
     public void display() {
