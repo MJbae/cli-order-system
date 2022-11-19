4d2003f69563c232a89f9834b856cd02ee9c3e91
MJbae <bmj5208@gmail.com>
2022-11-19 21:08:09 +0900

    test: 주문의 동시성 처리 테스트의 목적에 적절하게 기존 테스트 구조 변경

--- src/test/java/kr/co/_29cm/homework/OrderConcurrencyTest.java
+++ src/test/java/kr/co/_29cm/homework/OrderConcurrencyTest.java
@@ -1,9 +1,12 @@
 package kr.co._29cm.homework;
 
+import kr.co._29cm.homework.application.ItemService;
 import kr.co._29cm.homework.application.OrderService;
 import kr.co._29cm.homework.cli.OrderDto;
+import kr.co._29cm.homework.domain.Item;
 import kr.co._29cm.homework.exception.SoldOutException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,38 +16,56 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class OrderConcurrencyTest {
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private ItemService itemService;
 
     private final List<OrderDto> firstOrderDtos = new ArrayList<>();
 
     private final Long itemId = 778422L;
     private final int orderItemCount = 1;
+    private int stockQuantity;
 
     @BeforeEach
     void setUp() {
+        Item item = itemService.loadOne(itemId);
+        stockQuantity = item.getStockQuantity();
         firstOrderDtos.add(new OrderDto(itemId, orderItemCount));
     }
 
     @Test
-    void 동시적으로_복수의_주문이_들어오는_상황에서_재고가_부족해지면_재고부족_예외가_발생한다() throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(7);
-        CountDownLatch countDownLatch = new CountDownLatch(7);
+    @DisplayName("동시적으로 복수의 주문이 들어온다면 재고수만큼만 주문에 성공하고 나머지 주문에서는 재고부족 예외가 발생한다")
+    void test_in_concurrency_orders_success_only_under_stock_quantity_else_throw_exception() throws InterruptedException {
+        AtomicInteger successCount = new AtomicInteger();
+        int numberOfExecution = 10;
 
-        for (int i = 1; i <= 7; i++) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        CountDownLatch countDownLatch = new CountDownLatch(numberOfExecution);
+
+        for (int i = 1; i <= numberOfExecution; i++) {
             executorService.execute(() -> {
-                orderService.order(firstOrderDtos);
+                try{
+                    orderService.order(firstOrderDtos);
+                    successCount.getAndIncrement();
+                    System.out.println("성공");
+                } catch (SoldOutException soldOutException) {
+                    System.out.println("soldOutException");
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
                 countDownLatch.countDown();
             });
         }
 
         countDownLatch.await();
 
-        assertThatThrownBy(() -> orderService.order(firstOrderDtos))
-                .isInstanceOf(SoldOutException.class);
+        assertThat(successCount.get()).isEqualTo(stockQuantity);
     }
 }
