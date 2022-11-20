4e36ba25c2629ba177d52d6c85694498ddab2580
MJbae <bmj5208@gmail.com>
2022-11-20 21:55:56 +0900

    fix: 주문 상품에 대한 출력 로직에 배송비 출력 추가

--- src/main/java/kr/co/_29cm/homework/cli/printer/OrderProductPrinter.java
+++ src/main/java/kr/co/_29cm/homework/cli/printer/OrderProductPrinter.java
@@ -15,6 +15,10 @@ import java.util.List;
 public class OrderProductPrinter implements OrderItemPrinter<Order> {
     private final OrderProductService orderProductService;
 
+    private final BigDecimal deliveryFee = new BigDecimal(2500);
+
+    private final BigDecimal amountLimit = new BigDecimal(50000);
+
     @Override
     public void showBy(Order order) {
         List<OrderItem> orderItems = orderProductService.loadOneBy(order);
@@ -34,21 +38,27 @@ public class OrderProductPrinter implements OrderItemPrinter<Order> {
         System.out.println(getPricePayingMessage(order));
     }
 
-    private String getOrderItemMessage(OrderItem orderItem){
+    private String getOrderItemMessage(OrderItem orderItem) {
         return orderItem.getItem().getName() + " - " + orderItem.getCount() + "개";
     }
 
-    private String getOrderPriceMessage(List<OrderItem> orderItems){
+    private String getOrderPriceMessage(List<OrderItem> orderItems) {
         BigDecimal totalPrice = new BigDecimal(0);
-        for (OrderItem orderItem : orderItems){
+        for (OrderItem orderItem : orderItems) {
             BigDecimal priceSum = BigDecimal.valueOf(orderItem.getItem().getPrice().longValue() * orderItem.getCount());
             totalPrice = totalPrice.add(priceSum);
+        }
+
+        if (totalPrice.longValue() < amountLimit.longValue()) {
+            return "주문금액: " + totalPrice + "원 \n" +
+                    "배송비: " + deliveryFee.longValue();
+
         }
 
         return "주문금액: " + totalPrice + "원";
     }
 
-    private String getPricePayingMessage(Order order){
+    private String getPricePayingMessage(Order order) {
         return "지불금액: " + order.getPrice() + "원";
     }
 }
